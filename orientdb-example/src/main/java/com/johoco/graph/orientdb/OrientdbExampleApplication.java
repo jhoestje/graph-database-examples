c378f01f8578ea6b5c43a97a0ad545319bad86d7
John Hoestje <jhoestje@keyholesoftware.com>
2018-08-26 20:34:28 -0500

    commit orientdb start

---
+++ orientdb-example/src/main/java/com/johoco/graph/orientdb/OrientdbExampleApplication.java
@@ -0,0 +1,12 @@
+package com.johoco.graph.orientdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientdbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientdbExampleApplication.class, args);
+	}
+}
---
+++ orientdb-example/src/test/java/com/johoco/graph/orientdb/OrientdbExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.johoco.graph.orientdb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrientdbExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
