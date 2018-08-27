cbcbd77bc64ad6cf7c25a8096cc8d0f0792719e7
John Hoestje <jhoestje@keyholesoftware.com>
2018-08-26 20:27:30 -0500

    commit neo4j start

---
+++ neo4j-example/src/main/java/com/johoco/graph/neo4j/Neo4jExampleApplication.java
@@ -0,0 +1,12 @@
+package com.johoco.graph.neo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Neo4jExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Neo4jExampleApplication.class, args);
+	}
+}
