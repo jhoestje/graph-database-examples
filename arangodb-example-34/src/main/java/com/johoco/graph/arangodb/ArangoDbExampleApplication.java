603ae3404014593bea438ae44b1c00d9dc0fb5f8
John Hoestje <jhoestje@keyholesoftware.com>
2018-12-27 21:21:27 -0600

    adding a project for arangodb34

---
+++ arangodb-example-34/src/main/java/com/johoco/graph/arangodb/ArangoDbExampleApplication.java
@@ -0,0 +1,12 @@
+package com.johoco.graph.arangodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArangoDbExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArangoDbExampleApplication.class, args);
+	}
+}
