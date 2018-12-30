563a99b5bfdd282a30179b44a7c3baf176d075d8
John Hoestje <jhoestje@keyholesoftware.com>
2018-12-29 22:54:03 -0600

    added basic infrastructure

--- arangodb-example-34/src/main/java/com/johoco/graph/arangodb/ArangoDbExampleApplication.java
+++ arangodb-example-34/src/main/java/com/johoco/graph/arangodb/ArangoDbExampleApplication.java
@@ -2,8 +2,10 @@ package com.johoco.graph.arangodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.johoco.graph.arangodb")
 public class ArangoDbExampleApplication {
 
 	public static void main(String[] args) {
