e5320c4c4f1548e7bf782c7fd1ef7c982b2cc12d
John Hoestje <jhoestje@keyholesoftware.com>
2019-01-06 23:23:36 -0600

    updated repo config to just check edge defs

--- arangodb-example-34/src/main/java/com/johoco/graph/arangodb/config/RepoConfiguration.java
+++ arangodb-example-34/src/main/java/com/johoco/graph/arangodb/config/RepoConfiguration.java
@@ -1,6 +1,5 @@
 package com.johoco.graph.arangodb.config;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +7,13 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
 
-import com.arangodb.ArangoCollection;
 import com.arangodb.ArangoDB;
-import com.arangodb.ArangoDBException;
 import com.arangodb.ArangoDatabase;
 import com.arangodb.ArangoEdgeCollection;
 import com.arangodb.ArangoGraph;
+import com.arangodb.entity.CollectionType;
 import com.arangodb.entity.EdgeDefinition;
-import com.arangodb.entity.GraphEntity;
+import com.arangodb.model.CollectionCreateOptions;
 import com.arangodb.springframework.core.ArangoOperations;
 import com.johoco.graph.arangodb.model.ArtifactId;
 import com.johoco.graph.arangodb.model.Classifier;
@@ -54,24 +52,34 @@ public class RepoConfiguration { // implements ArangoConfiguration {
 			arangoDB.createDatabase("artifactDemo");
 			db = arangoDB.db("artifactDemo");
 		}
-		// check which collections need to be added... may be new vertices
-		if (!db.collection(GroupId.getVertexName()).exists()) {
-			db.createCollection(GroupId.getVertexName());
-		}
-		if (!db.collection(ArtifactId.getVertexName()).exists()) {
-			db.createCollection(ArtifactId.getVertexName());
-		}
-		if (!db.collection(Version.getVertexName()).exists()    ) {
-			db.createCollection(Version.getVertexName());
-		}
-		if (!db.collection(Packaging.getVertexName()).exists()  ) {
-			db.createCollection(Packaging.getVertexName());
-		}
-		if (!db.collection(Classifier.getVertexName()).exists() ) {
-			db.createCollection(Classifier.getVertexName());
-		}
-		
-		
+//		// check which collections need to be added... may be new vertices
+//		if (!db.collection(GroupId.getVertexName()).exists()) {
+//			db.createCollection(GroupId.getVertexName());
+//		}
+//		if (!db.collection(ArtifactId.getVertexName()).exists()) {
+//			db.createCollection(ArtifactId.getVertexName());
+//		}
+//		if (!db.collection(Version.getVertexName()).exists()) {
+//			db.createCollection(Version.getVertexName());
+//		}
+//		if (!db.collection(Packaging.getVertexName()).exists()) {
+//			db.createCollection(Packaging.getVertexName());
+//		}
+//		if (!db.collection(Classifier.getVertexName()).exists()) {
+//			db.createCollection(Classifier.getVertexName());
+//		}
+//
+//		// was there a way to do this by checking edge collection below?
+//		if (!db.collection(ArtifactIdOf.getEdgeName()).exists()) {
+//			db.createCollection(ArtifactIdOf.getEdgeName(), new CollectionCreateOptions().type(CollectionType.EDGES));
+//		}
+//		if (!db.collection(VersionOf.getEdgeName()).exists()) {
+//			db.createCollection(VersionOf.getEdgeName(), new CollectionCreateOptions().type(CollectionType.EDGES));
+//		}
+//		if (!db.collection(PackageOf.getEdgeName()).exists()) {
+//			db.createCollection(PackageOf.getEdgeName(), new CollectionCreateOptions().type(CollectionType.EDGES));
+//		}
+
 		// check if the graph needs to be created
 		ArangoGraph graph = db.graph("artifacts");
 
@@ -79,31 +87,23 @@ public class RepoConfiguration { // implements ArangoConfiguration {
 			db.createGraph("artifacts", null, null);
 			graph = db.graph("artifacts");
 		}
-		
-		
-		final EdgeDefinition artifactOfDefa = new EdgeDefinition().collection(ArtifactIdOf.getEdgeName())
-				.from(ArtifactId.getVertexName()).to(GroupId.getVertexName());
-		graph.addEdgeDefinition(artifactOfDefa);
 
-		// check which edges need to be created
-//		ArangoEdgeCollection add = null;//	graph.edgeCollection(ArtifactIdOf.getEdgeName()) == null
-//		add.
-		if (graph.edgeCollection(ArtifactIdOf.getEdgeName()) == null) {
+		Collection<String> existingEdgeDefs = graph.getEdgeDefinitions();
+
+		// check which edges need to be created; the edges and vertices will automatically be created if they don't exist
+		if (!existingEdgeDefs.contains(ArtifactIdOf.getEdgeName())) {
 			final EdgeDefinition artifactOfDef = new EdgeDefinition().collection(ArtifactIdOf.getEdgeName())
 					.from(ArtifactId.getVertexName()).to(GroupId.getVertexName());
 			graph.addEdgeDefinition(artifactOfDef);
 		}
 
-		if (graph.edgeCollection(VersionOf.getEdgeName()) == null) {
+		if (!existingEdgeDefs.contains(VersionOf.getEdgeName())) {
 			final EdgeDefinition versionOfDef = new EdgeDefinition().collection(VersionOf.getEdgeName())
 					.from(Version.getVertexName()).to(ArtifactId.getVertexName());
 			graph.addEdgeDefinition(versionOfDef);
 		}
 
-		
-		ArangoEdgeCollection ad = graph.edgeCollection(PackageOf.getEdgeName());
-//		ad.
-		if (graph.edgeCollection(PackageOf.getEdgeName()) == null) {
+		if (!existingEdgeDefs.contains(PackageOf.getEdgeName())) {
 			final EdgeDefinition packageOfDef = new EdgeDefinition().collection(PackageOf.getEdgeName())
 					.from(Packaging.getVertexName()).to(Version.getVertexName());
 			graph.addEdgeDefinition(packageOfDef);
